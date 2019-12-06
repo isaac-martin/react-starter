891d5526d160aff8883f8612e273767045143098
Isaac Martin <isaac.martin@fanduel.com>
2019-12-06 17:34:23 -0500

    broken mq

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Query from './components/Query';
 import {Router} from '@reach/router';
 
 const App = () => {
+  console.log(theme);
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
--- src/components/Base/styles.js
+++ src/components/Base/styles.js
@@ -1,10 +1,31 @@
 import styled from 'styled-components';
 import {padding} from 'polished';
+const mediaHelper = require('styled-media-helper');
 
-export const Title = styled.h1`
-  color: ${props => props.theme.palette.primary};
-  ${({theme}) => theme.breakpoints.phone`
-      background: red;
-   `}
-  ${padding('12px', '24px', '36px', '48px')}
-`;
+const media = mediaHelper({
+  sm: 320,
+  md: 768,
+  lg: 1240
+});
+
+// export const Title = styled.h1`
+//   color: ${props => props.theme.palette.primary};
+//   ${({theme}) => theme.breakpoints.phone`
+//       background: red;
+//    `}
+//   ${padding('12px', '24px', '36px', '48px')}
+// `;
+
+
+export const Title = styled.h1(({theme}) => ({
+  color: theme.palette.primary,
+  ...(theme.breakpoints.phone && {
+    background: `red`
+  }),
+  ...padding('12px', '24px', '36px', '48px'),
+  console.log(
+  ...(theme.breakpoints.phone && {
+    background: `red`
+  })
+);
+}));
