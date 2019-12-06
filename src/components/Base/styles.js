7714611fde73e28c1cc7fe4450dd750f2cfc21ba
Isaac Martin <isaac.martin@fanduel.com>
2019-12-06 17:01:16 -0500

    added polished

--- src/components/Base/styles.js
+++ src/components/Base/styles.js
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
+import {padding} from 'polished';
 
 export const Title = styled.h1`
   color: ${props => props.theme.palette.primary};
   ${({theme}) => theme.breakpoints.phone`
       background: red;
    `}
+  ${padding('12px', '24px', '36px', '48px')}
 `;
